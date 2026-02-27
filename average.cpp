9365d54407f4a2149aa5db9ec1e612e0bf8e8c1c
Ef <phurin.ef@gmail.com>
2026-02-27 15:12:23 +0700

    lab21 complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+
+int main(int argc,char *argv[]){
+    int sum,num;
+    if(argc < 2){
+        cout << "Please input numbers to find average.";
+    }else{
+        for(int i = 1;i<argc;i++){
+            num = atoi(argv[i]);
+            sum += num;
+        }
+        cout << "---------------------------------\n";
+        cout << "Average of "<< argc-1 <<" numbers = "<< sum/(argc-1);
+        cout << "\n---------------------------------";   
+    }
+}
